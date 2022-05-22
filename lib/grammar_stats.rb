266313de2660d34f3f4aa0effdd91a6506914034
Amir Khan <ak2022dev@gmail.com>
2022-05-22 14:15:39 +0100

    Completed Test drive single class program challenge.

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -7,6 +7,13 @@ class GrammarStats
   def check(text) # text is a string
     # Returns true or false depending on whether the text begins with
     # a captital letter and ends with a sentence-ending punctuation mark
-    text[0] == text[0].upcase && [ ".", "?", "!" ].include?(text[-1])
+    @count += 1
+    is_good = text[0] == text[0].upcase && [ ".", "?", "!" ].include?(text[-1])
+    @good_count += 1 if is_good
+    return is_good
+  end
+
+  def percentage_good
+    @count == 0 ? 0 : ((( @good_count / @count.to_f )) * 100).to_i
   end
 end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -1,8 +1,51 @@
 require 'grammar_stats'
 
 RSpec.describe GrammarStats do
-  it "Checks the percentage of grammatically good diary entries" do
-    stats = GrammarStats.new
-    
+  context "checks if text begins with capital letter and ends with valid punctuation" do
+    it "returns false if neither starts with capital nor ends with punctuation" do
+      gs = GrammarStats.new
+      expect(gs.check"ab").to eq false
+    end
+    it "returns false if starts with capital but does not end with punctuation" do
+      gs = GrammarStats.new
+      expect(gs.check"Ab").to eq false
+    end
+    it "returns false if doesn't start with capital but ends with valid punctuation " do
+      gs = GrammarStats.new
+      expect(gs.check"a.").to eq false
+    end
+    it "returns true if starts with capital and ends with valid punctuation" do
+      gs = GrammarStats.new
+      expect(gs.check"I.").to eq true
+    end
+  end  
+  context "reports percentage of texts so far that passed grammar check" do
+    it "returns 0 % if no checks done yet" do
+      gs = GrammarStats.new
+      expect(gs.percentage_good).to eq 0
+    end
+    it "returns 0 % if one check done and it was not good" do
+      gs = GrammarStats.new
+      gs.check("ab")
+      expect(gs.percentage_good).to eq 0
+    end
+    it "returns 100 % if one check done and it was good" do
+      gs = GrammarStats.new
+      gs.check("I!")
+      expect(gs.percentage_good).to eq 100
+    end
+    it "returns 50 % if two checks done and one was good" do
+      gs = GrammarStats.new
+      gs.check("ab")
+      gs.check("I!")
+      expect(gs.percentage_good).to eq 50
+    end
+    it "returns 66 % if three checks done and two were good (integer round down)" do
+      gs = GrammarStats.new
+      gs.check("I?")
+      gs.check("ba")
+      gs.check("I!")
+      expect(gs.percentage_good).to eq 66
+    end
   end
 end
